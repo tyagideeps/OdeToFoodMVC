5a3ee97985842b42069c00ec85c797c47aa8bdb6
Deepshikha Tyagi <deepshikha.tyagi@nagarro.com>
2020-04-23 03:44:39 +0530

    Final commit with completed application

--- OdeToFoodWeb/App_Start/BundleConfig.cs
+++ OdeToFoodWeb/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace OdeToFoodWeb
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap_yeti.css",
                       "~/Content/site.css"));
         }
     }
